de8373a4de8f39bdef0960e2b12daa966772c949
bogoreh <76124689+bogoreh@users.noreply.github.com>
2021-06-24 17:30:27 +0300

    The first commit

---
+++ 02_Tip calculator/Tip Calculator/js/index.js
@@ -0,0 +1,11 @@
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', function () {
+    let billAmount = document.getElementById('bill_amount').value;
+    let tipPercentage = document.getElementById('tip_percentage').value;
+
+    // console.log(billAmount);
+    let tipAmount = document.getElementById('tip_amount').value = billAmount / tipPercentage;
+    // document.getElementById('total_bill').value = parseFloat(billAmount) + parseFloat(tipAmount);
+    document.getElementById('total_bill').value = billAmount + tipAmount;
+});
